84100362a86f0e2a56727e7af1dfb007cd79a2bf
azulzaietz <azulzaietz@gmail.com>
2020-11-18 09:30:59 -0300

    +data base

--- http_processor.cpp
+++ http_processor.cpp
@@ -7,8 +7,16 @@ void HttpProcessor:: process(std::string s) {
     this->strstream << s;
 }
 
-std::string HttpProcessor:: answer() {
+std::string HttpProcessor:: answer(DataBase& data_base) {
+    //this->method = find_method();
+    this->data_base = data_base;
     return this->strstream.str();
 }
+
+/*
+HttpMethod* HttpProcessor:: find_method(char* method, char* recurse, char* protocol) {
+    return new HttpMethod();
+}
+*/
 
 HttpProcessor:: ~HttpProcessor() {}
--- http_processor.h
+++ http_processor.h
@@ -2,22 +2,28 @@
 #define HTTP_PROCESSOR_H
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include "http_method.h"
+#include "data_base.h"
 
 class HttpProcessor {
     private:
         std::stringstream strstream;
         DataBase data_base;
-        HttpMethod method;
+        HttpMethod* m;
+        char* method;
+        char* recurse;
+        char* protocol;
+        HttpMethod* find_method(char* method, char* recurse, char* protocol);
 
     public:
         HttpProcessor();
 
-        void process(std::string s, DataBase& data_base);
+        void process(std::string s);
 
-        std::string answer();
+        std::string answer(DataBase& data_base);
 
         ~HttpProcessor();
 };
