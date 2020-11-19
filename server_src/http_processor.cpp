2af834331be81e869d1b1926bd6bf2764bf31ca0
azulzaietz <azulzaietz@gmail.com>
2020-11-19 10:10:42 -0300

    servidor imprime primera linea peritorio

--- server_src/http_processor.cpp
+++ server_src/http_processor.cpp
@@ -11,10 +11,16 @@ const char* HttpProcessor:: answer(DataBase& data_base) {
     this->data_base = data_base;
     std::string word;
     unsigned wordCount = 0;
+    std::string first_line;
+
     while (this->strstream >> word) {
         if (wordCount == 0) this->method = word;
         if (wordCount == 1) this->recurse = word;
         if (wordCount == 2) this->protocol = word;
+        if (wordCount < 3) {
+            first_line += word;
+            first_line += " ";
+        }
         wordCount++;
         if (word == "START_BODY") {
             while (std::getline(this->strstream, word)) {
@@ -23,6 +29,8 @@ const char* HttpProcessor:: answer(DataBase& data_base) {
             }
         }
     }
+
+    std::cout << first_line.c_str() << '\n';
     this->m = find_method();
     return this->m->send_answer(data_base);
 }
